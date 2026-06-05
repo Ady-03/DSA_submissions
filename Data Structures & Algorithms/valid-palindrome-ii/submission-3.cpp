f832a12755d82b42f7121536d22e0afce3abcb44
Aditya Kain <147159903+Ady-03@users.noreply.github.com>
2026-06-05 17:33:10 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   private:
+    bool isPallindrome(int i, int j, string& s){
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+   public:
+    bool validPalindrome(string s) {
+        int i=0, j= s.length()-1;
+        while(i<j){
+            if(s[i] != s[j])
+                return isPallindrome(i+1,j,s) || isPallindrome(i,j-1,s);
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
